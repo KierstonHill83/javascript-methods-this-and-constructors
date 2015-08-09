6d175cc9ef86cc9623bd4b6a50061ce7cfc12cab
KierstonHill83 <khill1218@gmail.com>
2015-08-08 22:12:47 -0600

    Added functions for BookReader to pass all spec tests.

---
+++ js/book_reader.js
@@ -0,0 +1,34 @@
+function BookReader(book) {
+  this.book = book;
+  this.currentPage = 0;
+}
+
+BookReader.prototype.nextPage = function() {
+  if(this.currentPage < this.book.length - 1) {
+    this.currentPage++;
+  }
+  return this.book[this.currentPage];
+};
+
+BookReader.prototype.previousPage = function() {
+  if(this.currentPage > 0) {
+  this.currentPage--;
+  }
+  return this.book[this.currentPage];
+};
+
+BookReader.prototype.pagesLeft = function() {
+  return this.book.length - this.currentPage - 1;
+};
+
+BookReader.prototype.encouragement = function() {
+   this.currentPage++;
+  if(this.currentPage === this.book.length - 1) {
+    return "Keep going, this book is good 'til the last drop!";
+   } else {
+    return "Keep going, only 2 pages left after this one!";
+  }
+};
+
+// DO NOT MODIFY BELOW THIS COMMENT
+module.exports = BookReader;
